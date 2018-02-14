f9316d10a6434972dea88a5f9063cf65d5a2926d
Sergey Shalikin <s.erz.h@yandex.ru>
2018-02-14 11:17:34 +0300

    homework_6(allTestComplete)

--- src/store.js
+++ src/store.js
@@ -1,5 +1,14 @@
-import {createStore} from 'redux';
-import rootReducer from './reducers/CombineRedusers';
+import {createStore, applyMiddleware, compose} from 'redux';
+import rootReducer from './reducers';
+import showMiddleware from './middlewares/showMiddleware';
+import searchMiddleware from './middlewares/searchMiddleware';
 
 export default initialState =>
-  createStore(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(searchMiddleware, showMiddleware),
+      window.devToolsExtension ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    )
+  );
